a1dda1eb53a5940237aa92550f631d9be3e1583c
okonkwoernest <31658788+okonkwoernest@users.noreply.github.com>
2018-04-01 04:46:35 -0700

    firstweb

---
+++ index.php
@@ -0,0 +1,165 @@
+<?php
+	//Start session
+	session_start();
+	
+	//Unset the variables stored in session
+	unset($_SESSION['SESS_MEMBER_ID']);
+	unset($_SESSION['SESS_FIRST_NAME']);
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="WOW Slider, Carousel Slider jQuery, Page Slider jQuery" />
+    <meta name="description" content="WOWSlider created with WOW Slider, a free wizard program that helps you easily generate beautiful web slideshow" />
+    <!-- Start WOWSlider.com HEAD section -->
+    <link rel="stylesheet" type="text/css" href="engine1/style.css" media="screen" />
+    <style type="text/css">a#vlb{display:none}</style>
+    <script type="text/javascript" src="engine1/jquery.js"></script>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<title>Mamma Maria's Pizzeria</title>
+
+<meta name="keywords" content="free website templates, CSS layout, Pizza Company Website, HTML CSS" />
+
+<meta name="description" content="Pizza Company Website - free CSS website template, Free HTML CSS Layout" />
+
+<link href="templatemo_style.css" rel="stylesheet" type="text/css" />
+
+
+<link href="src/facebox.css" media="screen" rel="stylesheet" type="text/css" />
+ 
+  <script src="lib/jquery.js" type="text/javascript"></script>
+  <script src="src/facebox.js" type="text/javascript"></script>
+  <script type="text/javascript">
+    jQuery(document).ready(function($) {
+      $('a[rel*=facebox]').facebox({
+        loadingImage : 'src/loading.gif',
+        closeImage   : 'src/closelabel.png'
+      })
+    })
+  </script>
+  <STYLE TYPE="text/css">
+.personal{
+  text-align: center;
+  font-size: bold;
+  font-size: 16px;
+  color: white;
+  list-style-type: none;
+}
+
+  </STYLE>
+</head>
+<body>
+<div>
+
+
+
+</div>
+
+
+
+<div id="templatemo_container">
+  <div id="templatemo_header_section"> </div>
+  <div id="templatemo_menu_bg">
+    <div id="templatemo_menu">
+      <ul>
+        <li><a href="index.html"  class="current">Home</a></li>
+        <li><a href="aboutus.php">About Us</a></li>
+		<li><a href="contact.php">Location</a></li>
+        <li><a href="product.php">Product</a></li>
+        <li><a href="loginindex.php">Order Now! </a></li>
+        <li><a href="franchise.php">Franchise</a></li>
+      </ul>
+    </div>
+  </div>
+  <div ="templatemo_header_pizza"><!-- Start WOWSlider.com BODY section -->
+      <div id="wowslider-container1">
+          <div class="ws_images">
+              <a href="#"><img src="data1/images/img001.jpg" alt="img001" title="img001" id="wows0"/></a>
+              <a href="#"><img src="data1/images/img002.jpg" alt="img002" title="img002" id="wows1"/></a>
+              <a href="#"><img src="data1/images/img003.jpg" alt="img003" title="img003" id="wows2"/></a>
+              <a href="#"><img src="data1/images/img004.jpg" alt="img004" title="img004" id="wows3"/></a>
+              <a href="#"><img src="data1/images/img005.jpg" alt="img005" title="img005" id="wows4"/></a>
+              <a href="#"><img src="data1/images/img006.jpg" alt="img006" title="img006" id="wows5"/></a>
+              <a href="#"><img src="data1/images/img007.jpg" alt="img007" title="img007" id="wows6"/></a>
+              <a href="#"><img src="data1/images/img008.jpg" alt="img008" title="img008" id="wows7"/></a>
+              <a href="#"><img src="data1/images/img009.jpg" alt="img009" title="img009" id="wows8"/></a>
+              <a href="#"><img src="data1/images/img010.jpg" alt="img010" title="img010" id="wows9"/></a>
+              <a href="#"><img src="data1/images/img011.jpg" alt="img011" title="img011" id="wows10"/></a>
+              <a href="#"><img src="data1/images/img012.jpg" alt="img012" title="img012" id="wows11"/></a>
+              <a href="#"><img src="data1/images/img013.jpg" alt="img013" title="img013" id="wows12"/></a>
+              <a href="#"><img src="data1/images/img014.jpg" alt="img014" title="img014" id="wows13"/></a>
+              <a href="#"><img src="data1/images/img015.jpg" alt="img015" title="img015" id="wows14"/></a>
+              <a href="#"><img src="data1/images/img016.jpg" alt="img016" title="img016" id="wows15"/></a>
+              <a href="#"><img src="data1/images/img017.jpg" alt="img017" title="img017" id="wows16"/></a>
+              <a href="#"><img src="data1/images/img018.jpg" alt="img018" title="img018" id="wows17"/></a>
+              <a href="#"><img src="data1/images/img019.jpg" alt="img019" title="img019" id="wows18"/></a>
+              <a href="#"><img src="data1/images/img020.jpg" alt="img020" title="img020" id="wows19"/></a>
+              <a href="#"><img src="data1/images/img021.jpg" alt="img021" title="img021" id="wows20"/></a>
+              <a href="#"><img src="data1/images/img022.jpg" alt="img022" title="img022" id="wows21"/></a>
+              <a href="#"><img src="data1/images/img023.jpg" alt="img023" title="img023" id="wows22"/></a>
+              <a href="#"><img src="data1/images/img024.jpg" alt="img024" title="img024" id="wows23"/></a>
+              <a href="#"><img src="data1/images/img025.jpg" alt="img025" title="img025" id="wows24"/></a>
+              <a href="#"><img src="data1/images/unavalable.jpg" alt="unavalable" title="unavalable" id="wows25"/></a>
+          </div>
+          <div class="ws_bullets"><div>
+                  <a href="#wows0" title="img001"><img src="data1/tooltips/img001.jpg" alt="img001"/>1</a>
+                  <a href="#wows1" title="img002"><img src="data1/tooltips/img002.jpg" alt="img002"/>2</a>
+                  <a href="#wows2" title="img003"><img src="data1/tooltips/img003.jpg" alt="img003"/>3</a>
+                  <a href="#wows3" title="img004"><img src="data1/tooltips/img004.jpg" alt="img004"/>4</a>
+                  <a href="#wows4" title="img005"><img src="data1/tooltips/img005.jpg" alt="img005"/>5</a>
+                  <a href="#wows5" title="img006"><img src="data1/tooltips/img006.jpg" alt="img006"/>6</a>
+                  <a href="#wows6" title="img007"><img src="data1/tooltips/img007.jpg" alt="img007"/>7</a>
+                  <a href="#wows7" title="img008"><img src="data1/tooltips/img008.jpg" alt="img008"/>8</a>
+                  <a href="#wows8" title="img009"><img src="data1/tooltips/img009.jpg" alt="img009"/>9</a>
+                  <a href="#wows9" title="img010"><img src="data1/tooltips/img010.jpg" alt="img010"/>10</a>
+                  <a href="#wows10" title="img011"><img src="data1/tooltips/img011.jpg" alt="img011"/>11</a>
+                  <a href="#wows11" title="img012"><img src="data1/tooltips/img012.jpg" alt="img012"/>12</a>
+                  <a href="#wows12" title="img013"><img src="data1/tooltips/img013.jpg" alt="img013"/>13</a>
+                  <a href="#wows13" title="img014"><img src="data1/tooltips/img014.jpg" alt="img014"/>14</a>
+                  <a href="#wows14" title="img015"><img src="data1/tooltips/img015.jpg" alt="img015"/>15</a>
+                  <a href="#wows15" title="img016"><img src="data1/tooltips/img016.jpg" alt="img016"/>16</a>
+                  <a href="#wows16" title="img017"><img src="data1/tooltips/img017.jpg" alt="img017"/>17</a>
+                  <a href="#wows17" title="img018"><img src="data1/tooltips/img018.jpg" alt="img018"/>18</a>
+                  <a href="#wows18" title="img019"><img src="data1/tooltips/img019.jpg" alt="img019"/>19</a>
+                  <a href="#wows19" title="img020"><img src="data1/tooltips/img020.jpg" alt="img020"/>20</a>
+                  <a href="#wows20" title="img021"><img src="data1/tooltips/img021.jpg" alt="img021"/>21</a>
+                  <a href="#wows21" title="img022"><img src="data1/tooltips/img022.jpg" alt="img022"/>22</a>
+                  <a href="#wows22" title="img023"><img src="data1/tooltips/img023.jpg" alt="img023"/>23</a>
+                  <a href="#wows23" title="img024"><img src="data1/tooltips/img024.jpg" alt="img024"/>24</a>
+                  <a href="#wows24" title="img025"><img src="data1/tooltips/img025.jpg" alt="img025"/>25</a>
+                  <a href="#wows25" title="unavalable"><img src="data1/tooltips/unavalable.jpg" alt="unavalable"/>26</a>
+              </div></div>
+          <a style="display:none" href="http://wowslider.com">Ajax Slider jQuery by WOWSlider.com v1.7</a>
+      </div>
+      <script type="text/javascript" src="engine1/script.js"></script>
+      <!-- End WOWSlider.com BODY section -->
+  </div>
+  <div id="templatemo_content">
+    <div id="templatemo_content_left"><img src="images/main.png" width="729" height="312" style="margin-left:-10px;" /></div>
+    <div id="templatemo_card"></div>
+  </div>
+  <div id="templatemo_container_end"> </div>
+</div>
+<div id="templatemo_footer">
+    	<div class="top"></div>
+        <div class="middle">
+        Copyright © Crunchies FastFoodRestaurant</div>
+
+        <ul class="personal">
+<li>NAME: OTA JOSHUA EMMANUEL</li>
+<LI>MATRIC NUMBER: 2015/HDM//5917/CS</LI>
+<LI>DEPARTMENT:COMPUTER SCIENCE</LI>
+<LI>GROUP B</LI>
+
+        </ul>
+        <div class="button"></div>
+</div>
+<div>
+</div>
+</body>
+
+</html>
